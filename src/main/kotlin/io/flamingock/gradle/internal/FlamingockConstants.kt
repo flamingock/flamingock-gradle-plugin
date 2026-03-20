45a0d8b7243127c76dcf89ba00173707b777840e
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2026-03-20 09:20:05 +0800

    chore: bump version to 1.2.0-beta.2

--- src/main/kotlin/io/flamingock/gradle/internal/FlamingockConstants.kt
+++ src/main/kotlin/io/flamingock/gradle/internal/FlamingockConstants.kt
@@ -21,7 +21,7 @@ package io.flamingock.gradle.internal
 internal object FlamingockConstants {
     const val GROUP = "io.flamingock"
     const val EXTENSION_NAME = "flamingock"
-    const val FLAMINGOCK_VERSION = "1.2.0-beta.1"
+    const val FLAMINGOCK_VERSION = "1.2.0-beta.2"
 
     const val TEMPLATE_SQL_VERSION = "1.0.0-beta.1"
     const val TEMPLATE_MONGODB_VERSION = "1.0.0-beta.10"
